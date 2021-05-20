bb46586b12ecea014e9306ed69db6464b3c24730
Maite de la Roca <delaroca.estrada21@gmail.com>
2021-05-20 01:38:57 -0600

    sigmoid function

--- NeuralNetworks/model.py
+++ NeuralNetworks/model.py
@@ -5,25 +5,15 @@ def get_model():
 
     model = keras.models.Sequential()
 
-    model.add(Conv2D(64, kernel_size=(3,3), activation='relu', padding='same', input_shape=(9,9,1)))
+    model.add(Conv2D(64, kernel_size=(3,3), activation='sigmoid', padding='same', input_shape=(9,9,1)))
     model.add(BatchNormalization())
-    model.add(Conv2D(64, kernel_size=(3,3), activation='relu', padding='same'))
+    model.add(Conv2D(64, kernel_size=(3,3), activation='sigmoid', padding='same'))
     model.add(BatchNormalization())
-    model.add(Conv2D(128, kernel_size=(1,1), activation='relu', padding='same'))
-
+    model.add(Conv2D(128, kernel_size=(3,3), activation='sigmoid', padding='same'))
     model.add(Flatten())
     model.add(Dense(81*9))
     model.add(Reshape((-1, 9)))
     model.add(Activation('softmax'))
-
-    # model.add(Conv2D(32, (5, 5), input_shape=(1, 28, 28), activation='relu'))
-    # model.add(MaxPooling2D(pool_size=(2, 2)))model.add(Conv2D(16, (3, 3), activation='relu'))
-    # model.add(MaxPooling2D(pool_size=(2, 2)))
-    # model.add(Dropout(0.2))
-    # model.add(Flatten())
-    # model.add(Dense(128, activation='relu'))
-    # model.add(Dense(64, activation='relu'))
-    # model.add(Dense(num_classes, activation='softmax'))
 
     print(model.summary())
 
